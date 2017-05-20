fcbd334635e9e24cd5892bfda143fd042bff938b
Alice Maz <alice@alicemaz.com>
2017-05-20 23:31:35 +0000

    artist names

--- Main.hs
+++ Main.hs
@@ -1,7 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.List
+import Data.Semigroup
+import Data.Maybe
+import Control.Monad
 import System.Directory
-import System.FilePath.Posix
+import Text.Parsec
+
+artistName = do
+    name <- many1 $ noneOf "-_."
+    given <- (join . maybeToList . fmap (' ':)) <$> optionMaybe (char '-' >> artistName)
+    return (name <> given)
+
+tweetText f = parse artistName f f
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    filenames <- listDirectory "paintings"
+    --let Right testy = sequence $ tweetText <$> filenames
+    --putStrLn $ concatMap (++ "\n") $ nub testy
+    print $ tweetText <$> filenames
+    return ()
