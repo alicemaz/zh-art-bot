8c4dd634384d04b1d1dc5f5ef652f252ca535079
Alice Maz <alice@alicemaz.com>
2017-05-21 03:27:33 +0000

    weight down yang mingyi

--- Main.hs
+++ Main.hs
@@ -6,7 +6,7 @@ import Data.List
 import Data.Semigroup
 import Data.Maybe
 import Control.Monad
-import System.Random
+import Control.Monad.Random
 import System.Directory
 import Text.Parsec
 
@@ -14,19 +14,25 @@ name = unwords <$> sepBy1 (many1 lower) (char '-')
 
 filename = do
     artist <- name
-    char '_'
+    _ <- char '_'
     title <- try name <|> string "20.8.84"
     mSuffix <- optionMaybe $ char '_' >> (try name <|> many1 alphaNum)
     let suffix = join $ maybeToList $ (\s -> " (" <> s <> ")") <$> mSuffix
-    string ".jpg"
+    _ <- string ".jpg"
     return (artist <> " - " <> title <> suffix)
 
 parseFilename f = parse filename f f
 
+-- the hundred bridges series is 10% of the corpus lol
+weightFilename :: String -> (String, Rational)
+weightFilename f
+    | "yang-mingyi" `isPrefixOf` f = (f, 0.25)
+    | otherwise = (f, 1)
+
 main :: IO ()
 main = do
     files <- listDirectory "paintings"
-    file <- (files !!) <$> randomRIO (0, length files - 1)
+    file <- fromList $ weightFilename <$> files
     let Right parsed = parseFilename file
 
     putStrLn $ file ++ " / " ++ parsed
